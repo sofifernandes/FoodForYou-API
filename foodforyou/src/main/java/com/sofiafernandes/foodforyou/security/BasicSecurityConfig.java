a2d8c3f2234b959695a40d7ffd3c4da4bfe554d8
Sofia Fernandes <sofi-fernandes@live.com>
2023-05-07 18:19:11 -0300

    Ajuste importações BasicSecurityConfig

--- foodforyou/src/main/java/com/sofiafernandes/foodforyou/security/BasicSecurityConfig.java
+++ foodforyou/src/main/java/com/sofiafernandes/foodforyou/security/BasicSecurityConfig.java
@@ -2,7 +2,6 @@ package com.sofiafernandes.foodforyou.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
