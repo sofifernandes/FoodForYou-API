c99a4980aaf8985636cbbbade795339dda6fcde5
Sofia Fernandes <sofi-fernandes@live.com>
2023-11-23 20:14:53 -0300

    ajustando relacionamento

--- foodforyou/src/main/java/com/sofiafernandes/foodforyou/model/Tema.java
+++ foodforyou/src/main/java/com/sofiafernandes/foodforyou/model/Tema.java
@@ -29,7 +29,7 @@ public class Tema {
 	
 	private long qnt_posts;
 	
-	@OneToMany(mappedBy = "tema", cascade = CascadeType.ALL)
+	@OneToMany(mappedBy = "tema", cascade = CascadeType.REMOVE)
 	@JsonIgnoreProperties("tema")
 	private List<Postagem> postagem;
 
